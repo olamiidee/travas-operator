2dbae3b6754b4b84a5d9472d743b2b8fa7a17c01
Olamiidee <osibeluwoolamide@gmail.com>
2022-11-04 00:51:09 +0100

    home page commit

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="w-full h-20 bg-white px-[100px] fixed top-0 left-0 flex items-center z-[100] shadow-md">
+    <header className="w-full h-20 bg-white px-[50px] fixed top-0 left-0 flex items-center z-[100] shadow-md">
       <Link to="/" className="mr-[150px]">
-        <div className="w-20 h-10 border-[2px] border-blue-500 flex items-center gap-[0px] ml-[-10px] cursor-pointer">
+        <div className="w-20 h-10 border-[2px] border-blue-500 flex items-center gap-[0px] cursor-pointer">
           LOGO
           {/*<img alt="logo" src={logo} className="w-[60px] h-[60px]" />
           <p className="font-dyna text-[1.5rem] text-rose-300 tracking-widest">
@@ -36,7 +36,7 @@ const Header = () => {
       </nav>
       <div>
         <button className="bg-white font-[700] text-[0.90rem] text-blue-500 mr-[20px] px-[20px] py-[5px] border-2 border-blue-500 rounded-md hover:bg-blue-500 hover:text-white hover:translate-y-[6px] transition-all duration-300">
-          Login In
+          Log In
         </button>
         <button className="bg-blue-500 font-[700] text-[0.90rem] text-white px-[20px] py-[5px] rounded-md border-2 border-blue-500 hover:bg-blue-500 hover:translate-y-[6px] transition-all duration-300">
           Sign Up
--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,152 @@
 const Main = () => {
   return (
-    <div className="pt-[90px]">
-      <h1 className="text-[3rem] m-6">This is the main</h1>
+    <div className="pt-[90px] px-[50px]">
+      <div className="flex items-center space-x-4">
+        <img src="/images/nigeria-heart-flag.svg" alt="nigeria-heart-flag" />
+        <p> Perfectly designed for domestic tour operators within Nigeria</p>
+      </div>
+      {/* Slider Section */}
+      <div></div>
+      {/* Slider Section Ends */}
+      <div className="bg-[#3F3D56] pt-12 pb-20 space-y-28 mb-16">
+        {/* About Section*/}
+        <div className="w-[90%] mx-auto flex items-start text-white">
+          <img src="/images/rectangle.svg" alt="rectangle"></img>
+
+          <div className="pl-6 space-y-12">
+            {/* About */}
+            <div className="space-y-4">
+              <h4 className="pt-2 text-xl font-semibold">About</h4>
+              <p className="text-lg">
+                Lorem ispum et tu, Lorem ispum et tu Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum
+                et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu
+                ,Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum
+                et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu,
+                Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum
+                et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu,
+                Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu
+              </p>
+            </div>
+            {/* Why choose travas */}
+            <div className="space-y-4">
+              <h5 className="text-[#1F66D0] font-semibold text-xl">
+                Why choose Travas?
+              </h5>
+              <p className="text-lg">
+                Lorem ispum et tu, Lorem ispum et tu Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum
+                et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu
+                ,Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem
+                i
+              </p>
+            </div>
+            {/* More Sales */}
+            <div className="space-y-4">
+              <h4 className="text-xl font-semibold">More sales</h4>
+              <p className="text-lg">
+                Lorem ispum et tu, Lorem ispum et tu Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum
+                et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu
+                ,Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem
+                i, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu,
+                Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu
+              </p>
+            </div>
+            {/* Tour requests */}
+            <div className="space-y-4">
+              <h4 className="text-xl font-semibold">Tour requests</h4>
+              <p className="text-lg">
+                Lorem ispum et tu, Lorem ispum et tu Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum
+                et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu
+                ,Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem
+                i, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu,
+                Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu
+              </p>
+            </div>
+            {/* Manage bookings */}
+            <div className="space-y-4">
+              <h4 className="text-xl font-semibold">Manage bookings</h4>
+              <p className="text-lg">
+                Lorem ispum et tu, Lorem ispum et tu Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum
+                et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu
+                ,Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem
+                i, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu,
+                Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Price Section */}
+        <div className="w-[90%] mx-auto flex items-start text-white">
+          <img src="/images/rectangle.svg" alt="rectangle"></img>
+
+          <div className="pl-6 space-y-12">
+            {/* Pricing */}
+            <div className="space-y-4">
+              <h4 className="pt-2 text-xl font-semibold">Pricing</h4>
+              <p className="text-lg">
+                Lorem ispum et tu, Lorem ispum et tu Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum
+                et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu
+                ,Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum
+                et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu,
+                Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum
+                et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu,
+                Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Support Section */}
+        <div className="w-[90%] mx-auto flex items-start text-white">
+          <img src="/images/rectangle.svg" alt="rectangle"></img>
+
+          <div className="pl-6 space-y-12">
+            {/* Support */}
+            <div className="space-y-4">
+              <h4 className="pt-2 text-xl font-semibold">Support</h4>
+              <p className="text-lg">
+                Lorem ispum et tu, Lorem ispum et tu Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum
+                et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu
+                ,Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum
+                et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu,
+                Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem
+                ispum et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum
+                et tu, Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu,
+                Lorem ispum et tu, Lorem ispum et tu, Lorem ispum et tu
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="mx-auto w-fit text-center text-white">
+          <p className="text-white text-xl font-semibold">
+            Become a Travas supplier for free today!
+          </p>
+          <button className="bg-[#1F66D0] px-28 py-3 mt-4 mb-6 font-semibold">
+            Sign up for free
+          </button>
+          <p>
+            Already have an account?
+            <a href="" className="text-[#1F66D0]">
+              Login
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
